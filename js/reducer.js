4e1b08de12a314e0b8be64a50458da57a49ec387
Abdullah Bajaman <ambajaman@gmail.com>
2019-11-29 12:38:38 +0000

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,27 @@
 // add code snippets from README
+const defaultState = {count: 0}
+let state;
+const reducer = (state = defaultState.count, action) => {
+    switch(action.type){
+        case 'INCREASE_COUNT':
+            return state + 1
+
+        default:
+            return state
+    }
+}
+
+const dispatch = (action) =>{
+    state = reducer(state, action)
+    render()
+}
+
+const render = () => {
+    document.querySelector('#container').innerText = state
+}
+
+document.querySelector('#button').addEventListener('click', ()=> {
+    dispatch({type: 'INCREASE_COUNT'})
+})
+
+dispatch({type: '@@INIT'})
